d52cd25d0269463d0d8d8a07e20445ba3b755777
Jonathan Jeroen Beekman <beekm@re-stud-146-50-230-72.wireless.uva.nl>
2018-01-17 14:17:14 +0100

    Agent debugged

--- ecosystem/agent.py
+++ ecosystem/agent.py
@@ -16,7 +16,7 @@ class Patch(Agent):
     def step(self):
         # create a neighbor array
         neighbors = self.model.grid.get_neighbors(self.pos, moore=False)  # von Neumann neighborhood
-
+        print(len(neighbors))
         # calculate the amount of neighbors
         num_veg = 0
         for neighbor in neighbors:
@@ -27,17 +27,22 @@ class Patch(Agent):
         # calculate rates:
         w_mor = self.model.m
         w_deg = self.model.d
-        w_col = (self.model.delta * self.model.rho_veg + (1 - self.model.delta) * (q / len(neighbors))) * (
-                self.model.b - self.model.c * self.model.rho_veg) # w_0_plus, colonization
+        w_col = (
+                    self.model.delta * self.model.rho_veg +
+                    (1 - self.model.delta) * q
+                ) * \
+                (self.model.b - self.model.c * self.model.rho_veg) # w_0_plus, colonization
         w_reg = self.model.r + self.model.f * q  # w_-_0, regeneration
 
         # apply rules
         if self.condition == "Empty":
-            rand_num = random.random()
-            if rand_num < w_col:  # probability
-                self.condition = "Vegetated"
-            elif rand_num < w_col + w_deg:
-                self.condition = "Degraded"
+            states = ["Vegetated", "Degraded"]
+            t = [random.random() < w_col,
+                 random.random() < w_deg]
+            n = random.choice([0, 1])
+            new_state = states[n]
+            if t[n]:  # probability
+                self.condition = new_state
 
         elif self.condition == "Degraded":
             rand_num = random.random()
